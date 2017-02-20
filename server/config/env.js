db32875c29506cbd41b4a9f8f198e037d57fc581
zmudaj <zmudakann@gmail.com>
2017-02-20 15:01:05 -0700

    trying again

--- server/config/env.js
+++ server/config/env.js
@@ -3,10 +3,10 @@ let env = {
 	NODE_ENV: 'development',
 	PORT: 3000,
 	DBPROTOCOL: 'mongodb',
-	DBUSERNAME: 'student',
-	DBPASSWORD: 'CodeWorksStudent',
-	DBHOST: 'ds056789.mlab.com:56789',
-	DBNAME: 'codeworks',
+	DBUSERNAME: 'vue-kanban',
+	DBPASSWORD: 'vue-kanban',
+	DBHOST: 'ds056979.mlab.com:56979',
+	DBNAME: 'vue-kanban',
 	SERVERNAME: 'dev-server'
 }
 
